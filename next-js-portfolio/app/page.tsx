fb49cd2d0ada5f34dba9f11a7f461c169fa790d2
Sampo Riikkilä <78560347+Nerfoni@users.noreply.github.com>
2023-08-25 20:34:01 +0300

    Change timeline to be more reusable

--- next-js-portfolio/app/page.tsx
+++ next-js-portfolio/app/page.tsx
@@ -1,5 +1,5 @@
 import { Progress } from "@/components/ui/progress";
-import { EmploymentTimeline } from "@/components/employment-timeline/EmploymentTimeline";
+import { Timeline } from "@/components/timeline/Timeline";
 import { getEmployments } from "@/contentful/employments";
 import ColorModeButton from "@/components/ColorMode";
 
@@ -7,7 +7,7 @@ export default async function Home() {
   const employments = await getEmployments();
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-10 sm:p-24">
+    <main className="sm: flex min-h-screen flex-col items-center p-10 px-10 sm:py-24">
       <div className="flex flex-col items-center justify-center">
         <h1 className="pb-4 text-6xl font-bold">Sampo Riikkilä</h1>
         <p className="text-xl">Work History</p>
@@ -15,16 +15,20 @@ export default async function Home() {
       </div>
 
       <section className="work-history max-w-7xl">
-        <EmploymentTimeline>
-          {employments &&
-            employments.map((employment, index) => (
-              <EmploymentTimeline.Item
-                key={employment.employer}
-                employment={employment}
-                side={index % 2 === 0 ? "left" : "right"}
-              />
-            ))}
-        </EmploymentTimeline>
+        <Timeline>
+          {employments?.map((employment, index) => (
+            <Timeline.Item
+              key={`${employment.employer}-${index}`}
+              from={employment.from}
+              to={employment.to}
+              title={employment.employer}
+              subtitle={employment.position}
+              description={employment.description}
+              skills={employment.skills}
+              side={index % 2 === 0 ? "left" : "right"}
+            />
+          ))}
+        </Timeline>
         <Progress value={75} />
       </section>
 
---
+++ next-js-portfolio/components/timeline/Timeline.tsx
@@ -0,0 +1,74 @@
+import { Employment, Skill } from "@/contentful/employments";
+import { ChevronRight } from "lucide-react";
+import React from "react";
+import { Badge } from "../ui/badge";
+
+const Timeline = ({ children }: { children?: React.ReactNode }) => (
+  <div className="relative my-16">
+    <div className="z-1 employment-timeline__line absolute left-0 top-0 h-full w-1 translate-x-[-50%] bg-foreground lg:left-[50%]" />
+    {children}
+    <div className="absolute bottom-0 left-[calc(0%-0.375rem)] h-1 w-3 rounded-full bg-foreground lg:left-[calc(50%-0.375rem)]" />
+  </div>
+);
+
+Timeline.Item = ({
+  from,
+  to,
+  title,
+  subtitle,
+  description,
+  skills,
+  side,
+}: {
+  from: Date;
+  to?: Date;
+  title: string;
+  subtitle?: string;
+  description?: string;
+  skills?: Skill[];
+  side: "left" | "right";
+}) => {
+  // https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+  const margin = side === "left" ? "lg:mr-[50%]" : "lg:ml-[50%]";
+  const textAlign = side === "left" ? "lg:text-right" : "lg:text-left";
+  const justify = side === "left" ? "lg:justify-end" : "lg:justify-start";
+
+  const sortedSkills = skills?.sort((a, b) => {
+    if (a.favourite && !b.favourite) return -1;
+    if (!a.favourite && b.favourite) return 1;
+    return 0;
+  });
+
+  return (
+    <div className="relative">
+      <div className="absolute left-[calc(0%-0.375rem)] top-0 h-1 w-3 rounded-full bg-foreground lg:left-[calc(50%-0.375rem)]" />
+
+      <div className={`relative ${textAlign} px-16 py-4 sm:text-left ${margin} sm:m-0`}>
+        <div className="date-range mb-2 inline-flex items-center text-2xl">
+          <h2>{from.toLocaleDateString("en-US", { month: "short", year: "numeric" })}</h2>
+          <ChevronRight className="mx-2 text-black dark:text-purple-300" width="1rem" />
+          <h2>{to ? to.toLocaleDateString("en-US", { month: "short", year: "numeric" }) : "Present"}</h2>
+        </div>
+        <h3 className="employer mb-2 mt-3 text-2xl font-extralight">{title}</h3>
+        {subtitle && <h4 className="position text-lg font-bold">{subtitle}</h4>}
+        {description && <p className="description my-3 font-extralight">{description}</p>}
+        {skills && (
+          <div className={`skills mt-6 flex flex-wrap sm:justify-start ${justify}`}>
+            {sortedSkills?.map((skill) => {
+              return (
+                <div className="mb-2 mr-2">
+                  <Badge variant={skill.favourite ? "hightlighted" : "default"}>
+                    <p className="mr-1">{skill.title}</p>
+                    {skill.favourite && <p>&#10024;</p>}
+                  </Badge>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { Timeline };
